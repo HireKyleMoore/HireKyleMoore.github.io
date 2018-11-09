812b31a1bd728679af0a4e5fdcb4632f6ab979f8
Kyle Moore <kmoore1@scu.edu>
2018-11-08 18:27:56 -0800

    .

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(function () {
 });
 
 // Add scrollspy to <body>
-$('body').scrollspy({target: ".navbar", offset: 50});
+$('body').scrollspy({target: ".navbar", offset: 200});
 
 // Add smooth scrolling on all links inside the navbar
 $("#myNavbar a").on('click', function(event) {
